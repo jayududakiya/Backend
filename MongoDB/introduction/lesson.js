82c89eaa6e4107cfc5e3813d66483579aedacd2e
JD <137084651+jayududakiya@users.noreply.github.com>
2024-07-21 18:59:24 +0530

    introduction of MongoDB

---
+++ MongoDB/introduction/lesson.js
@@ -0,0 +1,15 @@
+// * connect MongoDB server in 
+// ! CMD
+// ? mongosh
+// ! MongoDB Server Start PROT Is : 27017
+
+// * show DataBase 
+// ? show dbs
+
+// * show collections in DataBase
+// ? show collections
+
+/*
+# WHAT IS BSON ? : 
+* => "Binary JSON" BSON's binary structure encodes tupe and length information, which allows it to be traversed much more quickly compared to JSON.
+*/ 
